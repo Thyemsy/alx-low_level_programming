6b32d0af6ecc507f106236b10695b0f212c31325
Thyemsy <thyemsyk@gmail.com>
2022-09-09 14:04:28 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() -  RAND_MAX / 2;
 
-        int lastDgit = n % 10;
+        int lastDigit = n % 10;
 	printf("Last digit of %d is %d ", n, lastDigit);
 	if (lastDigit > 5)
 		printf("and is greater than 5\n");
