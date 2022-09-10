b1abfe5af9524703e0e0940455f716b9021f48b6
Thyemsy <thyemsyk@gmail.com>
2022-09-09 17:58:10 -0700

    software is r=eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main- Write a program that prints all possible combinations of two two-digit numbers.
+ *Return: return 0
+ */
+ int main(void)
+{
+	int j, i;
+
+	for (j = 0;  <= 99; j++)
+	{
+		for (i = j; i <= 99; i++)
+			if (i != j)
+			{
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+
+				if (j *100 + 1 != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+	putchar('\n');
+	return (0);
+}
