675f5749f766f774845720b56b7de2b1213e742d
Thyemsy <thyemsyk@gmail.com>
2022-09-13 15:34:15 -0700

    Done with task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -print alphabet in lower case
+ * Return: Aways return 0
+ */
+voit print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		-putchar(i);
+	}
+	_putchar('\n');
+}
