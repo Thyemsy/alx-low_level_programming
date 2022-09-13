21a6be327b2741d84579239d28b96c41c56b4b90
Thyemsy <thyemsyk@gmail.com>
2022-09-13 16:48:00 -0700

    Done with tasj 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check if charater c is in lowercase and return the 1 else  return 0
+ * Return: Always return the value of 0
+ */
+int _islower(int c)
+{
+	c = _islower('c');
+	_putchar(c + '0');
+	c = _islower('o');
+	_putchar(c + 'C');
+	c = _islower(108);
+	_putchar(c + '0');
+	_putchar('\n');
+	return (0);
+}
