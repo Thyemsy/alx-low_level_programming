dbb941eeec027bc504735d39a5d99f5c61560ebe
Thyemsy <thyemsyk@gmail.com>
2022-09-14 03:07:09 -0700

    Done with task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * times_table - times table function
  */
@@ -12,6 +13,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			int prod = j * i;
+
 			if (j == 0)
 			{
 				_putchar('0');
@@ -28,7 +30,6 @@ void times_table(void)
 				_putchar(prod / 10 + '0');
 				_putchar(prod % 10 + '0');
 			}
-			
 		}
 		_putchar('\n');
 	}
