b62ba06300449e848aed881bdd10594b76ecec7e
Thyemsy <thyemsyk@gmail.com>
2022-09-14 03:18:31 -0700

    Done with task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ * main- Entry point
+ * add - add up function
+ * Return:return the sum of two integer
+ */
+int add(int a, int b)
+	int sum = a + b;
+{
+	return (sum);
+}
