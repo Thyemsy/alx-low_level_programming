2d62bab6fe714d4336a176a07574b58b47250a26
Thyemsy <thyemsyk@gmail.com>
2022-09-13 17:00:34 -0700

    Done with tasj 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,12 +6,14 @@
  */
 int _islower(int c)
 {
-	c = _islower('c');
-	_putchar(c + '0');
-	c = _islower('o');
-	_putchar(c + 'C');
-	c = _islower(108);
-	_putchar(c + '0');
+	if (c  = _islower('c'))
+	{
+		_putchar(c + 'c');
+		return(1);
+	}
+	else
+	{
+		return (0);
+	}
 	_putchar('\n');
-	return (0);
 }
