44f812971f8eec6fbfabbe64c63c644fffbd5a3a
Thyemsy <thyemsyk@gmail.com>
2022-09-13 17:17:07 -0700

    Done with tasj 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
 /**
  * _islower -if char c is in lowercase and return the 1 else  return 0
  * Return: Always return the value of 0
+ * @c: The character to check
  */
 int _islower(int c)
 {
-	if (c <= 'a' && c <= 'z')
+	if (c => 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
