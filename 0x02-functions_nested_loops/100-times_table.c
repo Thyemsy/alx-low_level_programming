fd2c436ecfc66f9cb82c8446ab54dfc3cfb34f33
Thyemsy <thyemsyk@gmail.com>
2022-09-14 05:54:53 -0700

    Done with Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - times table function
  * @n: integer to return
@@ -7,7 +8,7 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-	
+
 	if (n <= 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
@@ -15,7 +16,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				int prod = j * i;
-				
+
 				if (j == 0)
 				{
 					_putchar('0');
