a291ca01d69519fcfe7c272f63c923dcb5c06484
Thyemsy <thyemsyk@gmail.com>
2022-09-15 16:24:40 -0700

    Done with task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,12 @@
  * print_numbers - print number  bweteen 0 and 9 exclude 2 & 4
  *
  */
+
 void print_numbers(void)
 {
 	int i;
-	for (i = '0'; i < '10'; i++)
+
+	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
 	}
