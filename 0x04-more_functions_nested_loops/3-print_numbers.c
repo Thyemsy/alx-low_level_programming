d45a9574d59d4b48fff2d0833382a92a9f59af21
Thyemsy <thyemsyk@gmail.com>
2022-09-15 15:40:14 -0700

    Done with task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number  bweteen 0 and 9 exclude 2 & 4
+ * return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		for (i = 0; i != 2 && i = 4; i++)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
