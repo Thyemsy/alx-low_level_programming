e079f9d2d4cca49757ebbca22d1edd64187abe9a
Thyemsy <thyemsyk@gmail.com>
2022-09-19 12:58:22 -0700

    Done with task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen -function that return the length of a string
+ * @s: pointer valiable
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
