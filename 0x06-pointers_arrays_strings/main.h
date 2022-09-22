40f1a2aba59be5d4d5ce74e8891f1e1182f3d30e
Thyemsy <thyemsyk@gmail.com>
2022-09-22 03:51:45 -0700

    Done with task 3

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,12 +2,11 @@
 #define FILE_MAIN
 
 void print_number(int n);
-
-
-
-
-
-
-
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+void print_array(int *a, int n);
 
 #endif
